5047cd8fe5d537fcca8f409b554e2c0b92dce095
ju5tu5 <justus@ju5tu5.nl>
2022-12-12 10:07:51 +0100

    Voorbeeld uit de les

---
+++ assets/script/main.js
@@ -0,0 +1,8 @@
+let interaction = document.querySelector('a:nth-of-type(12)')
+
+interaction.addEventListener('click', jumpHandler)
+interaction.addEventListener('animationend', jumpHandler)
+
+function jumpHandler() {
+  interaction.classList.toggle('jump')
+}
