23a60f86073d0b1d46465a809092e76a88882651
Robin <robin.django@gmail.com>
2023-12-05 11:12:52 +0100

    tweaked shake sensitivity

--- assets/script/main.js
+++ assets/script/main.js
@@ -212,7 +212,7 @@ function permission() {
         if (response == "granted") {
           window.addEventListener("devicemotion", (e) => {
             // Shake sensitivity (a lower number is more)
-            var sensitivity = 50;
+            var sensitivity = 250;
 
             // Position variables
             var x1 = 0,
