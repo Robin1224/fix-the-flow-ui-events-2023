d5be8cc33115cbbad0154d45c64cc3388c3b9f32
Robin <robin.django@gmail.com>
2023-12-05 11:10:46 +0100

    refactored shake listener

--- assets/script/main.js
+++ assets/script/main.js
@@ -210,44 +210,7 @@ function permission() {
       .then((response) => {
         // (optional) Do something after API prompt dismissed.
         if (response == "granted") {
-          window.addEventListener("devicemotion", (e) => {
-            // Shake sensitivity (a lower number is more)
-            var sensitivity = 400;
-
-            // Position variables
-            var x1 = 0,
-              y1 = 0,
-              z1 = 0,
-              x2 = 0,
-              y2 = 0,
-              z2 = 0;
-
-            // Listen to motion events and update the position
-            window.addEventListener(
-              "devicemotion",
-              function (e) {
-                x1 = e.accelerationIncludingGravity.x;
-                y1 = e.accelerationIncludingGravity.y;
-                z1 = e.accelerationIncludingGravity.z;
-              },
-              false
-            );
-
-            // Periodically check the position and fire
-            // if the change is greater than the sensitivity
-            setInterval(function () {
-              var change = Math.abs(x1 - x2 + y1 - y2 + z1 - z2);
-
-              if (change > sensitivity) {
-                alert("Shake!");
-              }
-
-              // Update new position
-              x2 = x1;
-              y2 = y1;
-              z2 = z1;
-            }, 150);
-          });
+          shakeListener();
         }
       })
       .catch(console.error);
@@ -255,6 +218,46 @@ function permission() {
     alert("DeviceMotionEvent is not defined");
   }
 }
+
+const shakeListener = () => {
+  // Shake sensitivity (a lower number is more)
+  var sensitivity = 400;
+
+  // Position variables
+  var x1 = 0,
+      y1 = 0,
+      z1 = 0,
+      x2 = 0,
+      y2 = 0,
+      z2 = 0;
+
+  // Listen to motion events and update the position
+  window.addEventListener(
+    "devicemotion",
+    function (e) {
+      x1 = e.accelerationIncludingGravity.x;
+      y1 = e.accelerationIncludingGravity.y;
+      z1 = e.accelerationIncludingGravity.z;
+    },
+    false
+  );
+
+  // Periodically check the position and fire
+  // if the change is greater than the sensitivity
+  setInterval(function () {
+    var change = Math.abs(x1 - x2 + y1 - y2 + z1 - z2);
+
+    if (change > sensitivity) {
+      alert("Shake!");
+    }
+
+     // Update new position
+    x2 = x1;
+    y2 = y1;
+    z2 = z1;
+  }, 150);
+};
+
 const permissionBtn = document.getElementById("permission");
 permissionBtn.addEventListener("click", permission);
 
